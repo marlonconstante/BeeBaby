c755e179040db9d6331a9a2d9f8c02b170012a71
Marlon Santos Constante <marlon.constante@gmail.com>
2014-04-02 12:00:25 -0300

    Criação do delegate responsável por definir um zoom do mapa

---
+++ BeeBaby/BeeBaby/ZoomMapViewDelegate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using MonoTouch.MapKit;
+using MonoTouch.CoreLocation;
+
+namespace BeBabby
+{
+	public class ZoomMapViewDelegate : MKMapViewDelegate
+	{
+		private double zoom;
+
+		public ZoomMapViewDelegate(double zoom)
+		{
+			this.zoom = zoom;
+		}
+
+		public override void WillStartRenderingMap(MKMapView mapView)
+		{
+			changeZoomMap(mapView);
+		}
+
+		/// <summary>
+		/// Adjusts the zoom of the map.
+		/// </summary>
+		/// <param name="mapView">MapView.</param>
+		private void changeZoomMap(MKMapView mapView) {
+			if (mapView.UserLocation != null) {
+				CLLocationCoordinate2D coordinate = mapView.UserLocation.Coordinate;
+				if (coordinate.Latitude != 0f || coordinate.Longitude != 0f) {
+					MKCoordinateSpan span = new MKCoordinateSpan(zoom, zoom);
+					MKCoordinateRegion region = new MKCoordinateRegion(coordinate, span);
+					mapView.SetRegion(region, false);
+				}
+			}
+		}
+	}
+}
+
