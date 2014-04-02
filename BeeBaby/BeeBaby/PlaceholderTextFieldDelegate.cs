c1a005b8a05146bf7c012244d7cee0d3b79709be
Marlon Santos Constante <marlon.constante@gmail.com>
2014-04-02 11:59:01 -0300

    Adicionando delegate para o placeholder dos inputs (TextView e TextField)

---
+++ BeeBaby/BeeBaby/PlaceholderTextFieldDelegate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using MonoTouch.UIKit;
+
+namespace BeBabby
+{
+	public class PlaceholderTextFieldDelegate : UITextFieldDelegate
+	{
+		private Placeholder placeholder;
+
+		public PlaceholderTextFieldDelegate()
+		{
+			placeholder = new Placeholder();
+		}
+
+		public override void EditingStarted(UITextField textField)
+		{
+			textField.Text = placeholder.GetInitialText(textField.Text);
+		}
+
+		public override void EditingEnded(UITextField textField)
+		{
+			textField.Text = placeholder.GetFinalText(textField.Text);
+		}
+	}
+}
