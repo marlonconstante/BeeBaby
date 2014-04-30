8ffa7de6eda30960a3009a8aa9982a5f5fa3cb57
Giusepe Casagrande <giusepe@gmail.com>
2014-04-30 11:55:18 -0300

    Versão com problemas

---
+++ BeeBaby/Infrastructure.Repositories/SqliteNet/Entities/MomentsBabies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using SQLiteNetExtensions.Attributes;
+
+namespace Infrastructure.Repositories.SqliteNet.Entities
+{
+	public class MomentsBabies
+	{
+		[ForeignKey(typeof(MomentData))]
+		public string MomentId { get; set; }
+
+		[ForeignKey(typeof(BabyData))]
+		public string BabyId { get; set; }
+	}
+}
+
