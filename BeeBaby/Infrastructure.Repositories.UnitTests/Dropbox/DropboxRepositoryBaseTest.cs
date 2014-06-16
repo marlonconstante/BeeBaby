c2bdf802c75b25da703f03386f7e1d8ec1314acf
Giusepe Casagrande <giusepe@gmail.com>
2014-06-16 16:39:21 -0300

    Iniciada a integração com dropbox para coletar dados do usuário.

---
+++ BeeBaby/Infrastructure.Repositories.UnitTests/Dropbox/DropboxRepositoryBaseTest.cs
@@ -0,0 +1,111 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Skahal.Infrastructure.Framework.Repositories;
+using Domain.Moment;
+using Infrastructure.Repositories.Dropbox;
+
+namespace Infrastructure.Repositories.FunctionalTest.Repositories
+{
+    [TestFixture]
+    public class DropboxRepositoryBaseTest
+    {
+        [Test]
+        public void PersistNewItem_Item_PersistedOnDropbox()
+        {
+            var unitOfWork = new MemoryUnitOfWork();
+			var target = new DropboxRepositoryBase<Moment>("TEST");
+            target.ClearAll();
+            target.SetUnitOfWork(unitOfWork);
+
+			var moment = new Moment();
+			moment.Description = "TEST";
+			target.Add(moment);
+
+            var originalCount = target.CountAll();
+            unitOfWork.Commit();
+
+            Assert.AreEqual(originalCount + 1, target.CountAll());
+        }
+
+//        [Test]
+//        public void CountAll_Filter_FiltedResults()
+//        {
+//            var unitOfWork = new MemoryUnitOfWork();
+//            var target = new DropboxRepositoryBase<ContainsTextRule>("TEST");
+//            target.ClearAll();
+//            target.SetUnitOfWork(unitOfWork);
+//
+//            var rule = new ContainsTextRule("TEST_1");
+//            target.Add(rule);
+//
+//            rule = new ContainsTextRule("TEST_2");
+//            target.Add(rule);
+//
+//            unitOfWork.Commit();
+//
+//            Assert.AreEqual(2, target.CountAll());
+//            Assert.AreEqual(2, target.CountAll(c => c.Text.StartsWith("TEST")));
+//            Assert.AreEqual(2, target.CountAll(c => c.Text.Contains("_")));
+//            Assert.AreEqual(1, target.CountAll(c => c.Text.EndsWith("_1")));
+//            Assert.AreEqual(1, target.CountAll(c => c.Text.EndsWith("_2")));
+//        }
+//
+//        [Test]
+//        public void FindAll_Filter_FiltedResults()
+//        {
+//            var unitOfWork = new MemoryUnitOfWork();
+//            var target = new DropboxRepositoryBase<ContainsTextRule>("TEST");
+//            target.ClearAll();
+//            target.SetUnitOfWork(unitOfWork);
+//
+//            var rule = new ContainsTextRule("TEST_1");
+//            target.Add(rule);
+//
+//            rule = new ContainsTextRule("TEST_2");
+//            target.Add(rule);
+//
+//            unitOfWork.Commit();
+//
+//            Assert.AreEqual(2, target.FindAll().Count());
+//            Assert.AreEqual(2, target.FindAll(0, 2).Count());
+//            Assert.AreEqual(1, target.FindAll(0, 1).Count());
+//            Assert.AreEqual(1, target.FindAll(1, 2).Count());
+//            Assert.AreEqual(2, target.FindAll(c => c.Text.StartsWith("TEST")).Count());
+//            Assert.AreEqual(2, target.FindAll(c => c.Text.Contains("_")).Count());
+//            Assert.AreEqual(1, target.FindAll(c => c.Text.EndsWith("_1")).Count());
+//            Assert.AreEqual(1, target.FindAll(c => c.Text.EndsWith("_2")).Count());
+//        }
+//
+//        [Test]
+//        public void Pesists_Item_Persisted()
+//        {
+//            var unitOfWork = new MemoryUnitOfWork();
+//            var target = new DropboxRepositoryBase<ContainsTextRule>("TEST");
+//            target.ClearAll();
+//            target.SetUnitOfWork(unitOfWork);
+//
+//            var rule = new ContainsTextRule("TEST_1");
+//            target.Add(rule);
+//
+//            rule = new ContainsTextRule("TEST_2");
+//            target.Add(rule);
+//
+//            unitOfWork.Commit();
+//
+//            var actual = target.FindAll().ToList();
+//            Assert.AreEqual(2, actual.Count);
+//            Assert.AreEqual("TEST_1", actual[0].Text);
+//            Assert.AreEqual("TEST_2", actual[1].Text);
+//
+//            var actualRecord = actual[1];
+//            actualRecord.Text += " (UPDATED)";
+//            target[actualRecord.Id] = actualRecord;
+//            target.Remove(actual[0]);
+//            unitOfWork.Commit();
+//
+//            actual = target.FindAll().ToList();
+//            Assert.AreEqual(1, actual.Count);
+//            Assert.AreEqual("TEST_2 (UPDATED)", actual[0].Text);
+//        }
+    }
+}
---
+++ BeeBaby/Infrastructure.Repositories/Dropbox/DropboxRepositoryBase.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using BobbyTables;
+using HelperSharp;
+using Skahal.Infrastructure.Framework.Domain;
+using Skahal.Infrastructure.Framework.Repositories;
+
+namespace Infrastructure.Repositories.Dropbox
+{
+    /// <summary>
+    /// Repository using Dropbox.
+    /// </summary>
+    public class DropboxRepositoryBase<TEntity> : RepositoryBase<TEntity> where TEntity : EntityWithIdBase<string>, IAggregateRoot, new()
+    {
+        #region Fields
+        private string m_datastoreId;
+        private Datastore m_ds;
+        private Table<TEntity> m_table;
+        private bool m_initialized;
+        #endregion
+
+        #region Constructors
+        public DropboxRepositoryBase(string datastoreId)
+        {
+            ExceptionHelper.ThrowIfNull("datastoreId", datastoreId);
+
+            m_datastoreId = datastoreId;
+        }
+        #endregion
+
+        public override long CountAll(Expression<Func<TEntity, bool>> filter)
+        {
+            Initialize();
+
+            if (filter == null)
+            {
+                return m_table.Count();
+            }
+            else
+            {
+                return m_table.Count(filter.Compile());
+            }
+        }
+
+        public override IEnumerable<TEntity> FindAll(int offset, int limit, Expression<Func<TEntity, bool>> filter)
+        {
+            Initialize();
+
+            if (filter == null)
+            {
+                return m_table
+                    .Skip(offset)
+                    .Take(limit);
+            }
+            else
+            {
+                return m_table
+                    .Where(filter.Compile())
+                    .Skip(offset)
+                    .Take(limit);
+            }
+        }
+
+        public override IEnumerable<TEntity> FindAllAscending<TKey>(int offset, int limit, Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TKey>> orderBy)
+        {
+            Initialize();
+            return FindAll(offset, limit, filter).OrderBy(orderBy.Compile());
+        }
+
+        public override IEnumerable<TEntity> FindAllDescending<TKey>(int offset, int limit, Expression<Func<TEntity, bool>> filter, Expression<Func<TEntity, TKey>> orderBy)
+        {
+            Initialize();
+            return FindAll(offset, limit, filter).OrderByDescending(orderBy.Compile());
+        }
+
+        public override TEntity FindBy(object key)
+        {
+            Initialize();
+            return m_table.Get(key as string);
+        }
+
+        public void ClearAll()
+        {
+            Initialize();
+
+            foreach (var r in m_table)
+            {
+                m_table.Delete(r.Id);
+            }
+
+            PushToDropbox();
+        }
+
+        protected override void PersistDeletedItem(TEntity item)
+        {
+            Initialize();
+            m_table.Delete(item.Id);
+            PushToDropbox();
+        }
+
+        protected override void PersistNewItem(TEntity item)
+        {
+            Initialize();
+            m_table.Insert(item);
+            PushToDropbox();
+        }
+
+		async private void PushToDropbox()
+        {
+			if (!await m_ds.PushAsync())
+            {
+                throw new InvalidOperationException("Error syncing with DropBox.");
+            }
+        }
+
+        protected override void PersistUpdatedItem(TEntity item)
+        {
+            Initialize();
+            m_table.Update(item);
+            PushToDropbox();
+        }
+
+		async private void Initialize()
+        {
+            if (!m_initialized)
+            {
+                // https://www.dropbox.com/1/oauth2/authorize?client_id=5wqvkk9wlx8uhz9&response_type=code&redirect_uri=http://localhost
+				var manager = new DatastoreManager("cJBboFs_i-sAAAAAAAACt6z7uYpy-2cOt-NEONBjqQ1spYtW0nCbFn7_X3PQH502");
+				m_ds = await manager.GetOrCreateAsync(m_datastoreId.ToLowerInvariant());
+                m_table = m_ds.GetTable<TEntity>(typeof(TEntity).Name);
+				m_ds.PushAsync();
+                m_initialized = true;
+            }
+        }
+    }
+}
