6402df70d868b54a1a1b9dd07fc21f0da5183fc0
Giusepe Casagrande <giusepe@gmail.com>
2014-06-23 14:36:12 -0300

    Índices nos eventos

--- BeeBaby/Infrastructure.Repositories/SqliteNet/Entities/EventData.cs
+++ BeeBaby/Infrastructure.Repositories/SqliteNet/Entities/EventData.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using SQLiteNetExtensions.Attributes;
 using System.Collections.Generic;
+using SQLite.Net.Attributes;
 
 namespace Infrastructure.Repositories.SqliteNet.Entities
 {
@@ -16,18 +17,21 @@ namespace Infrastructure.Repositories.SqliteNet.Entities
 		/// Gets or sets the start age in days.
 		/// </summary>
 		/// <value>The start age.</value>
+		[Indexed]
 		public int StartAge	{ get; set; }
 
 		/// <summary>
 		/// Gets or sets the end age in days.
 		/// </summary>
 		/// <value>The end age.</value>
+		[Indexed]
 		public int EndAge { get; set; }
 
 		/// <summary>
 		/// Gets or sets the kind.
 		/// </summary>
 		/// <value>The kind.</value>
+		[Indexed]
 		public int Kind { get; set; }
 
 		[OneToMany]
@@ -37,12 +41,14 @@ namespace Infrastructure.Repositories.SqliteNet.Entities
 		/// Gets or sets the tag.
 		/// </summary>
 		/// <value>The tag.</value>
+		[Indexed]
 		public int Tag { get; set; }
 
 		/// <summary>
 		/// Gets or sets the Order.
 		/// </summary>
 		/// <value>The Order.</value>
+		[Indexed]
 		public int Priority { get; set; }
 	}
 }
