e188ba1b40370ac3d1a3f02833571cde8949b8be
Marlon Santos Constante <marlon.constante@gmail.com>
2014-06-26 16:13:22 -0300

    Adicionando o serviço do usuário

---
+++ BeeBaby/Domain/User/UserService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Skahal.Infrastructure.Framework.Domain;
+using Skahal.Infrastructure.Framework.Repositories;
+
+namespace Domain.User
+{
+	/// <summary>
+	/// User service.
+	/// </summary>
+	public class UserService : ServiceBase<User, IUserRepository, IUnitOfWork>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Domain.User.UserService"/> class.
+		/// </summary>
+		/// <param name="mainRepository">Main repository.</param>
+		/// <param name="unitOfWork">Unit of work.</param>
+		public UserService(IUserRepository mainRepository, IUnitOfWork unitOfWork)
+			: base(mainRepository, unitOfWork)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Domain.User.UserService"/> class.
+		/// </summary>
+		public UserService()
+		{
+		}
+
+		/// <summary>
+		/// Saves the user.
+		/// </summary>
+		/// <param name="user">User.</param>
+		public void SaveUser(User user)
+		{
+			MainRepository[user.Id] = user;
+			UnitOfWork.Commit();
+		}
+
+		/// <summary>
+		/// Gets the user.
+		/// </summary>
+		/// <returns>The user.</returns>
+		public User GetUser()
+		{
+			return MainRepository.FindFirst();
+		}
+	}
+}
