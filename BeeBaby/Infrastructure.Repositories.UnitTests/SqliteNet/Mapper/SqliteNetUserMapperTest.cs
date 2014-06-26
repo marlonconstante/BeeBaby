ea25f01c561ac5169b62e711959d5bbc87205398
Marlon Santos Constante <marlon.constante@gmail.com>
2014-06-26 15:45:23 -0300

    Adicionando o repositório do usuário

---
+++ BeeBaby/Infrastructure.Repositories.UnitTests/SqliteNet/Mapper/SqliteNetUserMapperTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using NUnit.Framework;
+using Infrastructure.Repositories.SqliteNet.Mapper;
+using Infrastructure.Repositories.SqliteNet.Entities;
+using Domain.Moment;
+using Domain.User;
+
+namespace Infrastructure.Repositories.UnitTests.SqliteNet.Mapper
+{
+	[TestFixture()]
+	public class SqliteNetUserMapperTest
+	{
+		[Test()]
+		public void ToRepository_Null_Null()
+		{
+			Assert.AreEqual(null, new SqliteNetUserMapper().ToRepositoryEntity(null));
+		}
+
+		[Test()]
+		public void ToDomainEntity_Null_Null()
+		{
+			Assert.AreEqual(null, new SqliteNetUserMapper().ToDomainEntity(null));
+		}
+
+		[Test()]
+		public void ToDomainEntity_RepositoryEntity_DomainEntity()
+		{
+			var repositoryEntity = new UserData()
+			{
+				Id = "1",
+				Email = "teste@teste.com"
+			};
+
+			var actual = new SqliteNetUserMapper().ToDomainEntity(repositoryEntity);
+			Assert.AreEqual(repositoryEntity.Id, actual.Id);
+			Assert.AreEqual(repositoryEntity.Email, actual.Email);
+		}
+
+		[Test()]
+		public void ToRepositoryEntity_DomainEntity_RepositoryEntity()
+		{
+			var domainEntity = new User()
+			{
+				Id = "1",
+				Email = "teste@teste.com"
+			};
+
+			var actual = new SqliteNetUserMapper().ToRepositoryEntity(domainEntity);
+			Assert.AreEqual(domainEntity.Id, actual.Id);
+			Assert.AreEqual(domainEntity.Email, actual.Email);
+		}
+	}
+}
---
+++ BeeBaby/Infrastructure.Repositories/SqliteNet/Entities/UserData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Infrastructure.Repositories.SqliteNet.Entities
+{
+	public class UserData : DataBase
+	{
+		/// <summary>
+		/// Gets or sets the email.
+		/// </summary>
+		/// <value>The email.</value>
+		public string Email
+		{
+			get;
+			set;
+		}
+	}
+}
