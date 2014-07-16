3500c29671f2bc55c05cdc6035ae6e890adbe768
Marlon Santos Constante <marlon.constante@gmail.com>
2014-07-16 13:56:45 -0300

    Identação

--- BeeBaby/BeeBaby/TimelineViewSource.cs
+++ BeeBaby/BeeBaby/TimelineViewSource.cs
@@ -130,9 +130,9 @@ namespace BeeBaby
 				var proxy = new EventProxy<TimelineViewSource, EventArgs>(this);
 				proxy.Action = (target, sender, args) => {
 					ActionProgress actionProgress = new ActionProgress(() => {
-					var momentImageView = (MomentImageView) sender;
-					target.m_viewController.PresentViewController(target.m_fullscreenController, false, null);
-					target.m_fullscreenController.SetInformation(momentImageView.Moment, CurrentContext.Instance.CurrentBaby, momentImageView.Photo);
+						var momentImageView = (MomentImageView) sender;
+						target.m_viewController.PresentViewController(target.m_fullscreenController, false, null);
+						target.m_fullscreenController.SetInformation(momentImageView.Moment, CurrentContext.Instance.CurrentBaby, momentImageView.Photo);
 					}, false);
 					actionProgress.Execute();
 				};
