f9b24710b83b8ed235f07dd3d24f610e02d4b989
Marlon Santos Constante <marlon.constante@gmail.com>
2014-08-05 11:29:24 -0300

    Adicionando domínios de fluxo

---
+++ BeeBaby/Domain/Log/Flow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Skahal.Infrastructure.Framework.Domain;
+
+namespace Domain.Log
+{
+	/// <summary>
+	/// Flow.
+	/// </summary>
+	public class Flow : EntityWithIdBase<string>, IAggregateRoot
+	{
+		/// <summary>
+		/// Gets or sets the device identifier.
+		/// </summary>
+		/// <value>The device identifier.</value>
+		public string DeviceId {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the session identifier.
+		/// </summary>
+		/// <value>The session identifier.</value>
+		public string SessionId {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		/// <value>The name.</value>
+		public string Name {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the date.
+		/// </summary>
+		/// <value>The date.</value>
+		public DateTime Date {
+			get;
+			set;
+		}
+	}
+}
---
+++ BeeBaby/Domain/Log/FlowService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Skahal.Infrastructure.Framework.Repositories;
+using Skahal.Infrastructure.Framework.Domain;
+
+namespace Domain.Log
+{
+	/// <summary>
+	/// Flow service.
+	/// </summary>
+	public class FlowService : ServiceBase<Flow, IFlowRepository, IUnitOfWork>
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Domain.Log.FlowService"/> class.
+		/// </summary>
+		/// <param name="mainRepository">Main repository.</param>
+		/// <param name="unitOfWork">Unit of work.</param>
+		public FlowService(IFlowRepository mainRepository, IUnitOfWork unitOfWork)
+			: base(mainRepository, unitOfWork)
+		{
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="Domain.Log.FlowService"/> class.
+		/// </summary>
+		public FlowService()
+		{
+		}
+
+		/// <summary>
+		/// Log the specified flow.
+		/// </summary>
+		/// <param name="flow">Flow.</param>
+		public void Log(Flow flow)
+		{
+		}
+	}
+}
