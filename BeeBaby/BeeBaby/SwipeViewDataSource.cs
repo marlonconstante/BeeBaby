533283fd1c343ad8c365c74c4f2fd481a56f7046
Marlon Santos Constante <marlon.constante@gmail.com>
2014-08-19 17:10:05 -0300

    Adicionando DataSource e Delegate padrão para o SwipeVie

---
+++ BeeBaby/BeeBaby/SwipeViewDataSource.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using BeeBaby.ViewModels;
+using MonoTouch.UIKit;
+using SwipeViewer;
+
+namespace BeeBaby
+{
+	public class SwipeViewDataSource : SwipeViewer.SwipeViewDataSource
+	{
+		public SwipeViewDataSource()
+		{
+			Images = new List<ImageModel>();
+		}
+
+		/// <summary>
+		/// Numbers the of items in swipe view.
+		/// </summary>
+		/// <returns>The of items in swipe view.</returns>
+		/// <param name="swipeView">Swipe view.</param>
+		public override int NumberOfItemsInSwipeView(SwipeView swipeView)
+		{
+			return Images.Count;
+		}
+
+		/// <summary>
+		/// Views the index of the for item at.
+		/// </summary>
+		/// <returns>The for item at index.</returns>
+		/// <param name="swipeView">Swipe view.</param>
+		/// <param name="index">Index.</param>
+		/// <param name="view">View.</param>
+		public override UIView ViewForItemAtIndex(SwipeView swipeView, int index, UIView view)
+		{
+			if (view == null)
+			{
+				view = new UIImageView() {
+					ContentMode = UIViewContentMode.ScaleAspectFit,
+					AutoresizingMask = UIViewAutoresizing.All,
+					Frame = swipeView.Bounds
+				};
+			}
+
+			var imageView = view as UIImageView;
+			imageView.Image = Images[index].Image;
+
+			return view;
+		}
+
+		/// <summary>
+		/// Gets or sets the images.
+		/// </summary>
+		/// <value>The images.</value>
+		public List<ImageModel> Images {
+			get;
+			protected set;
+		}
+	}
+}
---
+++ BeeBaby/BeeBaby/SwipeViewDelegate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using SwipeViewer;
+
+namespace BeeBaby
+{
+	public class SwipeViewDelegate : SwipeViewer.SwipeViewDelegate
+	{
+		public SwipeViewDelegate()
+		{
+		}
+
+		/// <summary>
+		/// Swipes the size of the view item.
+		/// </summary>
+		/// <returns>The view item size.</returns>
+		/// <param name="swipeView">Swipe view.</param>
+		public override SizeF SwipeViewItemSize(SwipeView swipeView)
+		{
+			return swipeView.Bounds.Size;
+		}
+
+		/// <summary>
+		/// Dids the index of the select item at.
+		/// </summary>
+		/// <param name="swipeView">Swipe view.</param>
+		/// <param name="index">Index.</param>
+		public override void DidSelectItemAtIndex(SwipeView swipeView, int index)
+		{
+			var viewController = Windows.GetTopViewController(swipeView.Window);
+			if (viewController is FullscreenViewController)
+			{
+				((FullscreenViewController) viewController).ShowOrHideSubviews();
+			}
+		}
+	}
+}
