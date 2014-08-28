6af2e50674ea5efab40c2903fce6c3cdfc74bb7b
Marlon Santos Constante <marlon.constante@gmail.com>
2014-08-28 17:03:54 -0300

    Adicionando interface IParseable (Classes que podem ser convertidas para um ParseObject)

---
+++ BeeBaby/Skahal.Infrastructure.Framework/Domain/IParseable.cs
@@ -0,0 +1,14 @@
+﻿namespace Skahal.Infrastructure.Framework.Domain
+{
+	/// <summary>
+	/// I parseable.
+	/// </summary>
+	public interface IParseable
+	{
+		/// <summary>
+		/// Gets the object identifier.
+		/// </summary>
+		/// <value>The object identifier.</value>
+		string ObjectId { get; }
+	}
+}
