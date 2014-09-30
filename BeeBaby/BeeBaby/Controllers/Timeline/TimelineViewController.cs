5e83b975b029ad558a7dce270e9582da3b8032db
Marlon Santos Constante <marlon.constante@gmail.com>
2014-09-30 09:44:25 -0300

    Comentando menu de sincronização do momento.

--- BeeBaby/BeeBaby/Controllers/Timeline/TimelineViewController.cs
+++ BeeBaby/BeeBaby/Controllers/Timeline/TimelineViewController.cs
@@ -205,7 +205,7 @@ namespace BeeBaby.Controllers
 
 				m_popover.AddPopoverItem("AddPhotos".Translate(), "photo", true, c_buttonHeight, proxyAddPhotos);
 				m_popover.AddPopoverItem("ChangeMoment".Translate(), "pencil", true, c_buttonHeight, proxyChangeMoment);
-				m_popover.AddPopoverItem("SyncMoment".Translate(), "sync", true, c_buttonHeight, proxySyncMoment);
+//				m_popover.AddPopoverItem("SyncMoment".Translate(), "sync", true, c_buttonHeight, proxySyncMoment);
 				m_popover.AddPopoverItem("RemoveMoment".Translate(), "trash", false, c_buttonHeight, proxyRemoveRow);
 
 				m_popover.AddSubviews(m_popover.MenuItems.ToArray());
