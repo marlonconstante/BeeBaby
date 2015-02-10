4793e92fef88bd9e9975f1ebe0bf6eb20053b9c9
Marlon Santos Constante <marlon.constante@gmail.com>
2015-02-10 08:58:49 -0200

    Adicionando extensão para o tipo ParseObject (Conversor)

---
+++ BeeBaby/Infrastructure.Systems/Utils/ParseExtension.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Parse;
+using System.Reflection;
+
+namespace Infrastructure.Systems.Utils
+{
+	public static class ParseExtension
+	{
+		/// <summary>
+		/// To the domain.
+		/// </summary>
+		/// <returns>The domain.</returns>
+		/// <param name="source">Source.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static T ToDomain<T>(this ParseObject source)
+		{
+			var target = Activator.CreateInstance<T>();
+			SetPropertyValue("ObjectId", target, source.ObjectId);
+			foreach (var key in source.Keys)
+			{
+				object value;
+				if (source.TryGetValue(key, out value))
+				{
+					SetPropertyValue(key, target, value);
+				}
+			}
+			return target;
+		}
+
+		/// <summary>
+		/// To the parse object.
+		/// </summary>
+		/// <returns>The parse object.</returns>
+		/// <param name="source">Source.</param>
+		/// <typeparam name="T">The 1st type parameter.</typeparam>
+		public static T ToParseObject<T>(this object source) where T : ParseObject
+		{
+			T target = (T) Activator.CreateInstance(typeof(T), source.GetType().Name);
+			foreach (var property in source.GetType().GetRuntimeProperties())
+			{
+				if (property.CanWrite)
+				{
+					var value = property.GetValue(source);
+					var propertyName = property.Name;
+					if ("ObjectId".Equals(propertyName))
+					{
+						target.ObjectId = (string) value;
+					}
+					else
+					{
+						target.Add(propertyName, value);
+					}
+				}
+			}
+			return target;
+		}
+
+		/// <summary>
+		/// Set the property value.
+		/// </summary>
+		/// <param name="name">Name.</param>
+		/// <param name="target">Target.</param>
+		/// <param name="value">Value.</param>
+		static void SetPropertyValue(string name, object target, object value)
+		{
+			var property = target.GetType().GetRuntimeProperty(name);
+			if (property != null)
+			{
+				property.SetValue(target, value);
+			}
+		}
+	}
+}
