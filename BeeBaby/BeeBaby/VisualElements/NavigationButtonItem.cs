00d48c60ed88f92f70a7b57c271719c1fa13bb9d
Marlon Santos Constante <marlon.constante@gmail.com>
2015-02-12 14:51:05 -0200

    Refactor para suportar botão como argumento do construtor

--- BeeBaby/BeeBaby/VisualElements/NavigationButtonItem.cs
+++ BeeBaby/BeeBaby/VisualElements/NavigationButtonItem.cs
@@ -8,36 +8,61 @@ namespace BeeBaby.VisualElements
 {
 	public class NavigationButtonItem : UIBarButtonItem
 	{
-		public NavigationButtonItem(RectangleF frame, float paddingLeft, string styleClass)
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BeeBaby.VisualElements.NavigationButtonItem"/> class.
+		/// </summary>
+		/// <param name="frame">Frame.</param>
+		/// <param name="paddingLeft">Padding left.</param>
+		/// <param name="styleClass">Style class.</param>
+		public NavigationButtonItem(RectangleF frame, float paddingLeft, string styleClass) : this(frame, CreateButton(frame, paddingLeft, styleClass))
 		{
-			AddCustomView(frame, paddingLeft, styleClass);
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="BeeBaby.VisualElements.NavigationButtonItem"/> class.
+		/// </summary>
+		/// <param name="frame">Frame.</param>
+		/// <param name="button">Button.</param>
+		public NavigationButtonItem(RectangleF frame, Button button)
+		{
+			Button = button;
+			AddCustomView(frame);
+		}
+
+		/// <summary>
+		/// Creates the button.
+		/// </summary>
+		/// <returns>The button.</returns>
+		/// <param name="frame">Frame.</param>
+		/// <param name="paddingLeft">Padding left.</param>
+		/// <param name="styleClass">Style class.</param>
+		static Button CreateButton(RectangleF frame, float paddingLeft, string styleClass)
+		{
+			frame.X += paddingLeft;
+
+			var button = new Button(frame);
+			button.SetStyleClass(styleClass);
+
+			return button;
 		}
 
 		/// <summary>
 		/// Adds the custom view.
 		/// </summary>
 		/// <param name="frame">Frame.</param>
-		/// <param name="paddingLeft">Padding left.</param>
-		/// <param name="styleClass">Style class.</param>
-		void AddCustomView(RectangleF frame, float paddingLeft, string styleClass)
+		void AddCustomView(RectangleF frame)
 		{
-			var buttonFrame = frame;
-			buttonFrame.X += paddingLeft;
-
-			Button = new Button(buttonFrame);
-			Button.SetStyleClass(styleClass);
-
 			CustomView = new View(frame);
 			CustomView.AddSubview(Button);
 		}
 
 		/// <summary>
-		/// Gets or sets the button.
+		/// Gets the button.
 		/// </summary>
 		/// <value>The button.</value>
 		public Button Button {
 			get;
-			set;
+			private set;
 		}
 
 		/// <summary>
