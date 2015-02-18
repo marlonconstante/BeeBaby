0e0db229791714024d2fce2a67112e7f5ae5c8f9
Marlon Santos Constante <marlon.constante@gmail.com>
2015-02-18 15:18:23 -0200

    Adicionando entidades do parse: UserAccount e UserFolder

---
+++ BeeBaby/BeeBaby/Synchronization/UserAccount.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using Skahal.Infrastructure.Framework.Domain;
+
+namespace BeeBaby.Synchronization
+{
+	public class UserAccount : IParseDomain, IParseUser, IFileRelease
+	{
+		public UserAccount()
+		{
+		}
+
+		/// <summary>
+		/// Gets or sets the object identifier.
+		/// </summary>
+		/// <value>The object identifier.</value>
+		public string ObjectId {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the created at.
+		/// </summary>
+		/// <value>The created at.</value>
+		public DateTime? CreatedAt {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the updated at.
+		/// </summary>
+		/// <value>The updated at.</value>
+		public DateTime? UpdatedAt {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the device identifier.
+		/// </summary>
+		/// <value>The device identifier.</value>
+		public string DeviceId {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the size.
+		/// </summary>
+		/// <value>The size.</value>
+		public long Size {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the version.
+		/// </summary>
+		/// <value>The version.</value>
+		public long Version {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the length of the files.
+		/// </summary>
+		/// <value>The length of the files.</value>
+		public long FilesLength {
+			get;
+			set;
+		}
+
+		/// <summary>
+		/// Gets or sets the length of the folders.
+		/// </summary>
+		/// <value>The length of the folders.</value>
+		public long FoldersLength {
+			get;
+			set;
+		}
+	}
+}
