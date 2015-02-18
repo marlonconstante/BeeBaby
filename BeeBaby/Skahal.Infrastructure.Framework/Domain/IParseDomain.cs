a709888553365a04cf0fbd57f137c870c6a36f86
Marlon Santos Constante <marlon.constante@gmail.com>
2015-02-18 14:42:29 -0200

    Interface que representa um domínio do Parse

---
+++ BeeBaby/Skahal.Infrastructure.Framework/Domain/IParseDomain.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Skahal.Infrastructure.Framework.Domain
+{
+	/// <summary>
+	/// Parse domain.
+	/// </summary>
+	public interface IParseDomain
+	{
+		/// <summary>
+		/// Gets or sets the object identifier.
+		/// </summary>
+		/// <value>The object identifier.</value>
+		string ObjectId { get; set; }
+
+		/// <summary>
+		/// Gets or sets the created at.
+		/// </summary>
+		/// <value>The created at.</value>
+		DateTime? CreatedAt { get; set; }
+
+		/// <summary>
+		/// Gets or sets the updated at.
+		/// </summary>
+		/// <value>The updated at.</value>
+		DateTime? UpdatedAt { get; set; }
+	}
+}
